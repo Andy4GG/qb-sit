5794ac7fb32adf8715df5769ef9ada64817b6b0f
AndyOnGitHub21 <109757479+AndyOnGitHub21@users.noreply.github.com>
2023-04-26 06:40:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# _sit_anywhere
+ A simple sit anywhere script for FiveM QBCore Framework Supported. Commented some of props but users can uncomment to use those prop too.
---
+++ fxmanifest.lua
@@ -0,0 +1,28 @@
+
+
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Andy.GG'
+description 'Sit Anywhere!'
+version '1.1.2'
+
+client_scripts {
+	'config.lua',
+	'list.lua',
+	'client.lua'
+}
+
+escrow_ignore {
+	'config.lua',
+	'list.lua',
+	'client.lua'
+}
+
+-- dependencies {
+-- 	'/server:5181'
+-- }
+
+-- dependency '/assetpacks'
